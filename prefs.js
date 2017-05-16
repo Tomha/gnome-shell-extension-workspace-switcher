1590242546bed00bab4792c9d4597fb778c9217a
Tomha <tomhah@protonmail.com>
2017-05-16 23:12:24 +1200

    Added abiltiy to rename workspaces from preferences dialog

--- prefs.js
+++ prefs.js
@@ -32,49 +32,75 @@ const MODES = ['modeCurrent', 'modeAll', 'modeIcon'];
 const POSITIONS = ['positionLeft', 'positionCenter', 'positionRight'];
 
 function WorkspaceSwitcherPrefs () {
-	this.init();
+    this.init();
 }
 
 WorkspaceSwitcherPrefs.prototype = {
-	init: function () {
-	    this._settings = Settings.getSettings();
+    init: function () {
+        this._settings = Settings.getSettings();
+        this._workspaceSettings = Settings.getSettings('org.gnome.desktop.wm.preferences');
 
-	    this._builder = new Gtk.Builder();
+        this._builder = new Gtk.Builder();
         this._builder.add_from_file(Me.path + '/prefs.ui');
 
         this.widget = this._builder.get_object('container');
         this._debug = this._builder.get_object('debug'); // DEBUG
         this._populate();
         this._builder.connect_signals_full(Lang.bind(this, this._signalConnector));
-	},
+    },
 
-	_populate: function () {
-	    let widget, value;
+    _populate: function () {
+        let widget, value;
 
         value = this._settings.get_int('index');
         widget = this._builder.get_object('index');
         widget.set_value(value);
 
-	    value = this._settings.get_enum('mode');
-	    widget = this._builder.get_object(MODES[value]);
-	    widget.set_active(true);
+        value = this._settings.get_enum('mode');
+        widget = this._builder.get_object(MODES[value]);
+        widget.set_active(true);
 
-	    value = this._settings.get_enum('position');
-	    widget = this._builder.get_object(POSITIONS[value]);
-	    widget.set_active(true);
+        value = this._settings.get_enum('position');
+        widget = this._builder.get_object(POSITIONS[value]);
+        widget.set_active(true);
 
-	    value = this._settings.get_boolean('use-names');
-	    widget = this._builder.get_object('useNames');
-	    widget.set_active(value);
+        value = this._settings.get_boolean('use-names');
+        widget = this._builder.get_object('useNames');
+        widget.set_active(value);
 
-	    value = this._settings.get_boolean('show-icon-text');
-	    widget = this._builder.get_object('showIconText');
-	    widget.set_active(value);
+        value = this._settings.get_boolean('show-icon-text');
+        widget = this._builder.get_object('showIconText');
+        widget.set_active(value);
 
-	    value = this._settings.get_boolean('invert-scrolling');
-	    widget = this._builder.get_object('invertScrolling');
-	    widget.set_active(value);
-	},
+        value = this._settings.get_boolean('invert-scrolling');
+        widget = this._builder.get_object('invertScrolling');
+        widget.set_active(value);
+
+        this._workspaceNameTreeView = this._builder.get_object('workspaceNameTreeView');
+        let column = new Gtk.TreeViewColumn({ title:"Name" });
+        let renderer = new Gtk.CellRendererText({ editable: true });
+        renderer.connect('edited', Lang.bind(this, this._signalHandler['onWorkspaceRenamed']));
+        column.pack_start(renderer, true);
+        column.add_attribute(renderer, 'text', 0);
+        this._workspaceNameTreeView.append_column(column);
+        this._workspaceNameListStore = this._builder.get_object('workspaceNameListStore');
+        let names = this._workspaceSettings.get_strv('workspace-names');
+        let [notEmpty, iter] = this._workspaceNameListStore.get_iter_first();
+        for (let i = 0; i < names.length; i++) {
+            iter = this._workspaceNameListStore.append();
+            this._workspaceNameListStore.set_value(iter, 0, names[i]);
+        }
+
+        let toolbar = this._builder.get_object('workspaceToolbar');
+        toolbar.get_style_context().add_class(Gtk.STYLE_CLASS_INLINE_TOOLBAR);
+        let addButton = new Gtk.ToolButton({icon_name: 'list-add-symbolic'});
+        addButton.connect('clicked', Lang.bind(this, this._signalHandler['onWorkspaceAdded']));
+        toolbar.add(addButton);
+        let delButton = new Gtk.ToolButton({icon_name: 'list-remove-symbolic'});
+        delButton.connect('clicked', Lang.bind(this, this._signalHandler['onWorkspaceRemoved']));
+        toolbar.add(delButton);
+
+    },
 
     _signalConnector: function (builder, object, signal, handler) {
         object.connect(signal, Lang.bind(this, this._signalHandler[handler]));
@@ -113,6 +139,41 @@ WorkspaceSwitcherPrefs.prototype = {
         onUseNamesChanged: function (toggleswitch) {
             this._settings.set_boolean('use-names', toggleswitch.get_active());
             this._settings.apply();
+        },
+
+        onWorkspaceAdded: function (button) {
+            let iter = this._workspaceNameListStore.append();
+            let workspaceNum = this._workspaceNameListStore.get_path(iter).get_indices()[0] + 1;
+            let workspaceName = 'Workspace ' + workspaceNum;
+            let names = this._workspaceSettings.get_strv('workspace-names');
+            names.push(workspaceName);
+            this._workspaceSettings.set_strv('workspace-names', names);
+            this._workspaceSettings.apply();
+            this._workspaceNameListStore.set_value(iter, 0, 'Workspace ' + workspaceNum);
+        },
+
+        onWorkspaceRenamed: function (renderer, path, text) {
+            let [iterSet, iter] = this._workspaceNameListStore.get_iter_from_string(path);
+            if (iterSet) {
+                let index = this._workspaceNameListStore.get_path(iter).get_indices()[0];
+                let names = this._workspaceSettings.get_strv('workspace-names');
+                names[index] = text;
+                this._workspaceSettings.set_strv('workspace-names', names);
+                this._workspaceSettings.apply();
+                this._workspaceNameListStore.set_value(iter, 0, text);
+            }
+        },
+
+        onWorkspaceRemoved: function (button) {
+            let [notEmpty, model, iter] = this._workspaceNameTreeView.get_selection().get_selected();
+            if (notEmpty) {
+                let index = this._workspaceNameListStore.get_path(iter).get_indices()[0];
+                let names = this._workspaceSettings.get_strv('workspace-names');
+                names.splice(index, 1);
+                this._workspaceSettings.set_strv('workspace-names', names);
+                this._workspaceSettings.apply();
+                this._workspaceNameListStore.remove(iter);
+            }
         }
     }
 }
